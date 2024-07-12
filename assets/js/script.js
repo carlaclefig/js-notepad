85660a9957dc9c83ad54dcd3d7ed885d98d1789f
Carla Clemente <carlaclefig@gmail.com>
2024-07-11 21:46:02 -0500

    chore: remove the title and color from the notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,17 @@
-// const noteObj = {
-//   title: "My new note",
-//   content: "This is the note content",
-//   color: "#EABAED",
-// };
-
 const noteArray = [];
 
 const notesListHtml = document.getElementById("notes-list");
 const noteCreatorHtml = document.getElementById("note-creator");
 
-function createNoteHtml(noteObj) {
+function createNoteHtml(content) {
   const noteHtml = document.createElement("article");
   noteHtml.classList.add("note");
-  noteHtml.style.backgroundColor = noteObj.color;
-
-  const noteHeader = document.createElement("header");
-
-  const noteTitle = document.createElement("h3");
-  noteTitle.classList.add("note__title");
-  noteTitle.innerText = noteObj.title;
-
-  noteHeader.append(noteTitle);
 
   const noteSection = document.createElement("section");
 
   const noteContent = document.createElement("p");
   noteContent.classList.add("note__content");
-  noteContent.innerText = noteObj.content;
+  noteContent.innerText = content;
 
   noteSection.append(noteContent);
 
@@ -34,34 +19,29 @@ function createNoteHtml(noteObj) {
   noteButton.classList.add("note__delete-btn"); //  <button class="note__delete-btn"></button>
   noteButton.innerText = "Delete"; //  <button class="note__delete-btn">Delete</button>
 
-  noteHtml.append(noteHeader, noteSection, noteButton);
+  noteHtml.append(noteSection, noteButton);
 
   return noteHtml;
 }
 
 function handlerSubmit(event) {
   event.preventDefault();
+
   const elements = event.target.elements;
 
-  const titleHtml = elements.title; // input name
-  const contentHtml = elements.content;
-  const colorHtml = elements.color;
+  const inputContentHtml = elements.content;
 
-  const note = {
-    title: titleHtml.value,
-    content: contentHtml.value,
-    color: colorHtml.value,
-  };
+  noteArray.push(inputContentHtml.value);
 
-  noteArray.push(note);
-  localStorage.setItem;
-  loadNotesToScreen();
+  addNoteToScreen(inputContentHtml.value);
+
+  inputContentHtml.value = "";
 }
 
 noteCreatorHtml.addEventListener("submit", handlerSubmit);
 
-function addNoteToScreen(note) {
-  const noteHtml = createNoteHtml(note);
+function addNoteToScreen(content) {
+  const noteHtml = createNoteHtml(content);
   notesListHtml.append(noteHtml);
 }
 
